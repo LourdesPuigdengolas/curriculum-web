f63c72ff0ba9b289a01550db5c1d41d8db573fef
María Lourdes Puigdengolas Lanciani <89802699+LourdesPuigdengolas@users.noreply.github.com>
2023-05-31 16:02:53 -0300

    eleminar js

--- funcione.js
+++
@@ -1,6 +0,0 @@
-
-// Funcion para mostrar el modal
-
-const myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
-// or
-const myModalAlternative = new bootstrap.Modal('#myModal', options)
